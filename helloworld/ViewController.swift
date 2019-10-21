c011b41af5c10ee5abece2427a65e30de9a7685c
s.kimura <s.kimura@creativehope.co.jp>
2019-10-21 11:09:01 +0900

    [swiftlint][update] swiftlintチェック用のコードを追加

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -15,6 +15,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func hoge(hoge:String)
+    {
+
+        let a=true
+        if a
+        {
+            print("hoge")
+        }
+        else
+        {
+            print("hoge");
+        }
+
+    }
 
 }
 
